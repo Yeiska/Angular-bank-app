7895d2901c0dca63414977d66ac580d3aab4372f
Yeiska De La Torre Fernandez <yeiska83@yahoo.com>
2022-01-23 14:11:06 -0500

    adding unit test to appComponent and styling to the table

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+import { Observable, of } from 'rxjs'
+
 import { AppComponent } from './app.component';
+import { AccountService } from './services/account.service';
+import { TRANSACTIONS } from './data';
+import { Account } from './models/account.model';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -7,25 +14,43 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [ HttpClientTestingModule ],
+      providers: [ 
+        { provide: AccountService, useClass: FakeAccountService }
+       ]
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'bank-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('bank-app');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('bank-app app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('bank-app is running!');
   });
 });
+
+class FakeAccountService {
+
+  ACCOUNT: Account = {
+    ownerName: "Jon",
+    ownerLastName: "Snow",
+    totalBalance: 2000,
+    transactions: TRANSACTIONS
+  }
+  getAccount(): Observable<Account> {
+      const account = of(this.ACCOUNT);
+      return account;
+  }
+}
